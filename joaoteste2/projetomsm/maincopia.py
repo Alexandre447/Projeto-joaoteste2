8c22325f51fb848f01c10c88204c11635d63a0f4
Alexandre447 <alexislima15a@gmail.com>
2025-07-09 19:17:27 -0300

    Delete joaoteste2 directory
    agr ta certo

--- joaoteste2/projetomsm/maincopia.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-from despesa import Despesa
-from tipo_despesa import TipoDespesa
-
-data = date(2025, 7, 2)
-impostos = Despesa("du guverno", 1533.28, data, TipoDespesa.Despesa)
-
-print(impostos)
